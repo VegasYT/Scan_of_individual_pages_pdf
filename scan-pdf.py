7552f71c7c7035700660336ade5142942c975bfa
VEGAS <51318919+VegasYT@users.noreply.github.com>
2023-05-19 00:47:00 +0300

    Update scan-pdf.py

--- scan-pdf.py
+++ scan-pdf.py
@@ -1,6 +1,13 @@
 import PyPDF2
+import fitz
+import pytesseract
+from PIL import Image
+from io import BytesIO
 
-pdf_path = 'document.pdf' # укажите путь к PDF-файлу здесь
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+pdf_path = 'document.pdf'
+doc = fitz.open(pdf_path)
 
 # Открываем PDF-файл и считываем первые две страницы (или меньше, если страниц меньше двух)
 with open(pdf_path, 'rb') as pdf_file:
@@ -10,3 +17,19 @@ with open(pdf_path, 'rb') as pdf_file:
         page = pdf_reader.getPage(i)
         print(page.extractText())
 
+image_xrefs = {}
+
+all_text = ""
+
+for i in range(min(2, len(doc))):
+    page = doc[i]
+    all_text += page.get_text()
+
+    for image in page.get_images():
+        image_xrefs.setdefault(image[0])
+
+for index, xref in enumerate(image_xrefs):
+    img = doc.extract_image(xref)
+    text = pytesseract.image_to_string(Image.open(BytesIO(img["image"])), lang="rus")
+
+    print(text)
